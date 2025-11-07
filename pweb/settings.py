892e24096cff80ec9a680220b2bca2cdd65f3b9a
Alvaro Eduardo <alvaroeduardo297@gmail.com>
2025-11-07 11:21:49 -0300

    07/11/2025 11:21

--- pweb/settings.py
+++ pweb/settings.py
@@ -27,7 +27,7 @@ LOGIN_URL = 'login'
 SECRET_KEY = config('SECRET_KEY') # <-- ALTERE AQUI
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
